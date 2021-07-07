1da3c5c61fb0654cbb0f6ee0ab71958f72388952
ORO-mlm <brandon2davincci@gmail.com>
2021-07-07 08:08:25 +0200

    checkpoint

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -73,7 +73,7 @@ static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits
  */
 static Checkpoints::MapCheckpoints mapCheckpoints = {
     { 0, uint256S("00000d7d6dcaecfdec619157642a912dfe2fabe053b9593212ce517a45270d01")},
-    // { 6900, uint256S("345c141975eed94b9fbdf429b507be4a20d09119e15e30a389c773fdf2891671")},
+    { 100, uint256S("000000ac10f9a81d89c7e9aad88826b378418eeecadbc3157a506796588dcaab")},
     // { 9930, uint256S("0d8f5da53764a7ea0485a75326264453679e8eada608ccbfb780a242cad96d1b")},
 };
 
